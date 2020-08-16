f5cfd61d6f51db3430e235b610612ad10459a885
mohammad najafi <5038849-mohammadnjf78@users.noreply.gitlab.com>
2020-08-16 13:00:16 +0430

    version-4

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,19 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 import { ApiOperation, ApiParam } from '@nestjs/swagger';
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
-  @ApiOperation({summary:'گرفتن یک عکس'})
-  @ApiParam({name:'fileId',description:'آدرس فایل'})
+  @ApiOperation({ summary: 'گرفتن یک عکس' })
+  @ApiParam({ name: 'fileId', description: 'آدرس فایل' })
   @Get('files/images/:fileId')
-  async serveAvatars(
+  serveAvatars(
     @Param('fileId') fileId: string,
     @Res() res: Response,
-  ): Promise<any> {
+  ): any {
     res.sendFile(fileId, { root: 'files/images' });
   }
 }

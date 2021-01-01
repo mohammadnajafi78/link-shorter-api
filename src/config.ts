f783c8cdea77e9dbaa4636965af37562dd735ccc
mohammad najafi <5038849-mohammadnjf78@users.noreply.gitlab.com>
2021-01-02 00:10:14 +0330

    new-version

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const MONGO_URI =
-  'mongodb+srv://turn-system:0atzD3gXX5BRJKHu@cluster0.oj9o5.mongodb.net/turn-system?authSource=admin&replicaSet=atlas-z0lu31-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true/link-shorter';
+  'mongodb+srv://turn-system:0atzD3gXX5BRJKHu@cluster0.oj9o5.mongodb.net/turn-system?retryWrites=true&w=majority';
 export const TOKEN_SECRET_KEY = 'NxA6trel4HXnk5DfpWxk';
 export const KAVENEGAR_API_KEY =
   '464D51696530496F30374E584330512F49786835416A326F44655479524A79375734477039595577314A493D';
